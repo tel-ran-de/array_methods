d3dd39477c88f0bf1ae9515db5c148415de9e8e4
callbackKid <vladislav.yarosh23@gmail.com>
2023-08-18 18:44:48 +0400

    form added

--- script.js
+++ script.js
@@ -1,48 +1,49 @@
 import { newId } from './utils.js'
 import { cartItems } from './products.js'
 
-let currentCartItems = cartItems.map((el) => ({ ...el, id: newId() })) // Create a separate array to store cart items
+// Добавляем в наш массив объектов id для товаров
+let currentCartItems = cartItems.map((el) => ({ ...el, id: newId() }))
 
+// Все селектор из html находятся в верзней части документа в одном месте
 const productContainer = document.getElementById('productsContainer')
 const buttonAscend = document.getElementById('ascButton')
 const buttonDescend = document.getElementById('descButton')
 const totalAmountContainer = document.getElementById('totalAmount')
 const totalPriceContainer = document.getElementById('totalPrice')
 
-let deleteButtons
+let toCartButtons
 
 const createNewArray = (arr) => {
   const newArr = arr
-
     .map(
       (product) => `
     <div class="productCard" data-id="${product.id}">
       <h2>${product.name}</h2>
       <p> price: ${product.price}</p>
       <p> amount: ${product.quantity}</p>
-      <button class="deleteButton" data-id="${product.id}">Delete</button>
-      <button class="basketButton" data-id="${product.id}">To cart</button>
+      <button class="cartButton" data-id="${product.id}">To cart</button>
     </div>`
     )
     .join('')
 
   productContainer.innerHTML = newArr
-  deleteButtons = document.querySelectorAll('.deleteButton')
-  attachDeleteButtonListeners()
-}
 
-createNewArray(currentCartItems)
+  // обращаемся к элементами после создания в DOM дереве
+  toCartButtons = document.querySelectorAll('.cartButton')
 
-function attachDeleteButtonListeners() {
-  deleteButtons.forEach((button) => {
-    button.addEventListener('click', (event) => {
-      const productId = event.target.getAttribute('data-id')
-      currentCartItems = currentCartItems.filter((el) => el.id !== productId)
-      createNewArray(currentCartItems)
+  // На каждую кнопку добавляем событие для добавления товара в корзину
+  toCartButtons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+      console.log(e.currentTarget)
+      console.log('aaa')
     })
   })
 }
 
+// вызываем функция для рендера элементов на странице
+createNewArray(currentCartItems)
+
+// Сортировка
 buttonAscend.addEventListener('click', () => {
   currentCartItems = [...currentCartItems].sort((a, b) => a.price - b.price)
   createNewArray(currentCartItems)
@@ -53,10 +54,13 @@ buttonDescend.addEventListener('click', () => {
   createNewArray(currentCartItems)
 })
 
+// Корзина
+
 const totalAmount = currentCartItems.reduce((acc, obj) => acc + obj.quantity, 0)
 totalAmountContainer.innerText = totalAmount
 const totalPrice = currentCartItems.reduce((acc, obj) => acc + obj.price * obj.quantity, 0)
 totalPriceContainer.innerText = totalPrice
 
-// Сделать форму
-// Сделать добавление в корзину
+// Домашнее задание:
+// написать функции, которые будут пересчитывать количество и сумму товаров
+// в корзине при добавлении товаров
