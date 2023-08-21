84f6ee1d8875fe1a05847410b3e5e22e70141044
callbackKid <vladislav.yarosh23@gmail.com>
2023-08-21 16:31:09 +0400

    cart update added

--- script.js
+++ script.js
@@ -8,11 +8,29 @@ let currentCartItems = cartItems.map((el) => ({ ...el, id: newId() }))
 const productContainer = document.getElementById('productsContainer')
 const buttonAscend = document.getElementById('ascButton')
 const buttonDescend = document.getElementById('descButton')
+
+// корзина
+const cart = []
+let toCartButtons
 const totalAmountContainer = document.getElementById('totalAmount')
 const totalPriceContainer = document.getElementById('totalPrice')
 
-let toCartButtons
+const countTotalAmount = (arr) => {
+  const totalAmount = arr.reduce((acc, obj) => acc + obj.quantity, 0)
+  totalAmountContainer.innerText = totalAmount
+}
 
+const countTotalPrice = (arr) => {
+  const totalPrice = arr.reduce((acc, obj) => acc + obj.price * obj.quantity, 0)
+  totalPriceContainer.innerText = totalPrice
+}
+
+countTotalAmount(cart)
+countTotalPrice(cart)
+
+// Функция которая рендерит список продуктов в корзине
+
+// Функция по созданию карточек продуктов на странице
 const createNewArray = (arr) => {
   const newArr = arr
     .map(
@@ -21,6 +39,7 @@ const createNewArray = (arr) => {
       <h2>${product.name}</h2>
       <p> price: ${product.price}</p>
       <p> amount: ${product.quantity}</p>
+      <button class="deleteButton" data-id="${product.id}">Delete</button>
       <button class="cartButton" data-id="${product.id}">To cart</button>
     </div>`
     )
@@ -31,13 +50,22 @@ const createNewArray = (arr) => {
   // обращаемся к элементами после создания в DOM дереве
   toCartButtons = document.querySelectorAll('.cartButton')
 
-  // На каждую кнопку добавляем событие для добавления товара в корзину
+  // Добавляем в корзину + пересчитываем количество и стоимость
   toCartButtons.forEach((button) => {
     button.addEventListener('click', (e) => {
-      console.log(e.currentTarget)
-      console.log('aaa')
+      // e.target - цель нашего события
+      // getAttribute - выбирает нужный аттрибут который мы хотим
+      const currentProductId = e.target.getAttribute('data-id')
+      // find находит нужный элемент по условию колбек функции !
+      const currentProduct = currentCartItems.find((product) => product.id === currentProductId)
+      cart.push(currentProduct) // добавляем обхект в корзину
+
+      countTotalAmount(cart) // обновляем переменную общего количества
+      countTotalPrice(cart) // обновляем цену
     })
   })
+
+  // Добавить кнопку удаления товара
 }
 
 // вызываем функция для рендера элементов на странице
@@ -54,13 +82,16 @@ buttonDescend.addEventListener('click', () => {
   createNewArray(currentCartItems)
 })
 
-// Корзина
-
-const totalAmount = currentCartItems.reduce((acc, obj) => acc + obj.quantity, 0)
-totalAmountContainer.innerText = totalAmount
-const totalPrice = currentCartItems.reduce((acc, obj) => acc + obj.price * obj.quantity, 0)
-totalPriceContainer.innerText = totalPrice
-
 // Домашнее задание:
 // написать функции, которые будут пересчитывать количество и сумму товаров
 // в корзине при добавлении товаров
+
+const arr = [
+  { name: 'olya', age: 29, id: 9 },
+  { name: 'Petya', age: 70, id: 10 },
+  { name: 'ivan', age: 90, id: 15 },
+]
+
+const v = arr.find((el) => el.id === 10)
+
+console.log(v)
