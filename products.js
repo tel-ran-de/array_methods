d7b45b36fc988c507dd8cefddc501858e8009dad
callbackKid <vladislav.yarosh23@gmail.com>
2023-08-23 16:33:03 +0400

    spread + find

--- products.js
+++ products.js
@@ -1,8 +1,8 @@
 export const cartItems = [
-  { name: 'Apple', price: 120, quantity: 3 },
-  { name: 'Banana', price: 50, quantity: 5 },
-  { name: 'Orange', price: 6, quantity: 2 },
-  { name: 'PineApple', price: 2, quantity: 2 },
+  { name: 'Apple', price: 120, inStock: 3 },
+  { name: 'Banana', price: 50, inStock: 5 },
+  { name: 'Orange', price: 6, inStock: 2 },
+  { name: 'PineApple', price: 2, inStock: 2 },
   { name: 'Coconut', price: 90, quantity: 2 },
   { name: 'Peach', price: 45, quantity: 2 },
   { name: 'Mango', price: 70, quantity: 4 },
